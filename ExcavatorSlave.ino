edc8f1ed31c4dc898b5a91871a45d25e0296057d
etka321123 <38590377+etka321123@users.noreply.github.com>
2018-05-20 22:53:52 +0300

    Add files via upload

---
+++ ExcavatorSlave.ino
@@ -0,0 +1,103 @@
+#include <SoftwareSerial.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+SoftwareSerial BTserial(10, 11);
+
+#define MIN_PULSE_WIDTH       650
+#define MAX_PULSE_WIDTH       2350
+#define DEFAULT_PULSE_WIDTH   1500
+#define FREQUENCY             50
+
+// called this way, it uses the default address 0x40
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+int pwm1 = 275;
+int state = 0;
+int x[6] = {};
+int i;
+uint8_t servonum = 0;
+uint8_t servonum1 = 5;
+uint8_t servonum2 = 2;
+
+uint8_t servonum9 = 9;
+uint8_t servonum10 = 10;
+uint8_t servonum11 = 11;
+
+int maxvalue =170;
+int minvalue = 30;
+int new0;
+
+void setup() {
+  // initialize digital pin 8 as an output.
+
+  Serial.begin(38400);
+  BTserial.begin(38400);  
+  pwm.begin();
+  pwm.setPWMFreq(60);
+  pinMode(13, OUTPUT);
+  // pwm.setPWM(servonum1, 0, pulseWidth(90)); 
+
+}
+int pulseWidth(int angle)
+{
+  int pulse_wide, analog_value;
+  pulse_wide   = map(angle, 0, 180, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
+  analog_value = int(float(pulse_wide) / 1000000 * FREQUENCY * 4096);
+  //Serial.println(analog_value);
+  return analog_value;
+}
+
+void loop() {
+
+ if(BTserial.available() > 0) {
+  int a = BTserial.read();
+  x[i]=a; 
+  i++;
+  if(a==0){
+    
+    Serial.println(map(x[0],0,240,minvalue,maxvalue));
+    Serial.println(map(x[1],0,240,minvalue,maxvalue));
+    Serial.println(map(x[2],0,240,minvalue,maxvalue));
+    Serial.println(x[3]);
+    Serial.println("-------------------------------");
+    i=0;
+  pwm.setPWM(servonum, 0, pulseWidth(map(x[0],0,240,minvalue,maxvalue)));
+  pwm.setPWM(servonum2, 0, pulseWidth(map(x[2],0,240,minvalue,maxvalue)));
+  pwm.setPWM(servonum1, 0, pulseWidth(map(x[1],0,240,0,maxvalue))); 
+  } 
+ 
+  if(x[3] == 1)  // DÜZ GİTME
+  {
+    pwm.setPWM(servonum11, 0,pulseWidth(120));   //ileri
+    pwm.setPWM(servonum10, 0, pulseWidth(60));  // ileri
+  }
+  if(x[3] == 2)  // GERİ GİTME
+  {
+    pwm.setPWM(servonum11, 0,pulseWidth(60));    //geri
+    pwm.setPWM(servonum10, 0,pulseWidth(120));  //geri
+  }
+  if(x[3] == 3)  // SAĞA GİTME
+  {
+    pwm.setPWM(servonum11, 0,pulseWidth(120));   //ileri
+    pwm.setPWM(servonum10, 0, pulseWidth(120)); //geri
+  }
+  if(x[3] == 4)  // SOLA GİTME
+  {
+    pwm.setPWM(servonum11, 0,pulseWidth(60));   //geri
+    pwm.setPWM(servonum10, 0, pulseWidth(60)); //ileri
+  }
+  if(x[3] == 5)
+  {
+     pwm.setPWM(servonum9, 0,pulseWidth(120)); 
+  }
+  if(x[3] == 6)
+  {
+     pwm.setPWM(servonum9, 0,pulseWidth(60)); 
+  }
+  if(x[3] == 100)
+  {
+    pwm.setPWM(servonum11, 0,pulseWidth(90));   //dur
+    pwm.setPWM(servonum10, 0, pulseWidth(90));  //dur
+  }
+  }
+  }
---
+++ GloveMaster.ino
@@ -0,0 +1,171 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(10, 11);
+//deneme sourcetree
+int state = 0;
+const int ledPin = 8;  //the pin your led is connected to
+const int buttonPin = 2;  //the pin your button is connected to
+int avg[4]={200,50,20,100};
+int buttonState = 0;
+unsigned long lastcom;
+unsigned long lastana;
+int i;
+int flex1min;//flex sensorler için max ve min değerleri
+int flex1max;
+int flex2min;
+int flex2max;
+int flex3min;
+int flex3max;
+int value1;
+int value2;
+int value3;
+int avgcount;
+ int avgvalue1;
+ int avgvalue2;
+ int avgvalue3;
+void setup() {
+  pinMode(8 , INPUT_PULLUP);
+  pinMode(2 , INPUT_PULLUP);
+  pinMode(3 , INPUT_PULLUP);
+  pinMode(4 , INPUT_PULLUP);
+  pinMode(5 , INPUT_PULLUP);
+  pinMode(6 , INPUT_PULLUP);
+  pinMode(7 , INPUT_PULLUP);
+  Serial.begin(9600);
+  BTserial.begin(38400);
+
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  Serial.println("kalibrasyon başlıyor");
+  Serial.println("elinizi düz tutun");
+  delay(2000);
+  int tempvalue1;
+  int tempvalue2;
+  int tempvalue3;
+   tempvalue1=tempvalue1 + analogRead(A0);
+   tempvalue2=tempvalue2 + analogRead(A1);
+   tempvalue3=tempvalue3 + analogRead(A2);
+  for(int i = 0; i < 20; i++ ){
+   tempvalue1=tempvalue1 + analogRead(A0);
+   tempvalue2=tempvalue2 + analogRead(A1);
+   tempvalue3=tempvalue3 + analogRead(A2);
+   delay(10);
+  }
+  flex1min = tempvalue1 / 20 ;
+  flex2min = tempvalue2 / 20 ;
+  flex3min = tempvalue3 / 20 ;
+  Serial.println(flex1min);
+  Serial.println(flex2min);
+  Serial.println(flex3min);
+  delay(1000);
+  Serial.println("elini bük");
+  delay(3000);
+  tempvalue1 = 0;
+  tempvalue2 = 0;
+  tempvalue3 = 0;
+  for(int i = 0; i < 20; i++ ){
+   tempvalue1=tempvalue1 + analogRead(A0);
+   tempvalue2=tempvalue2 + analogRead(A1);
+   tempvalue3=tempvalue3 + analogRead(A2);
+   delay(10);
+  }
+  flex1max = tempvalue1 / 20 ;
+  flex2max = tempvalue2 / 20 ;
+  flex3max = tempvalue3 / 20 ;
+  delay(1000);
+   
+  Serial.println(flex1max);
+  
+  Serial.println(flex2max);
+  
+  Serial.println(flex3max);
+
+
+}
+
+void loop() {
+
+ 
+if(millis()-lastcom>20){
+  if(i<5){
+    BTserial.write(avg[i]);
+    i++;
+    lastcom=millis();
+  }
+  else 
+  i=0;
+}
+
+if(millis()-lastana > 3){
+  
+  int tempvalue1=analogRead(A0);
+ // Serial.println(tempvalue1);
+  int tempvalue2=analogRead(A1);
+  int tempvalue3=analogRead(A2);
+//    Serial.println(tempvalue1);
+//  Serial.println(tempvalue2);
+//  Serial.println(tempvalue3);
+
+  //if(tempvalue1 < flex1max && tempvalue1 > flex1min ){
+    value1 = tempvalue1;
+   // Serial.println(value1);//}
+  if(tempvalue2 < flex2max && tempvalue2 > flex2min)
+    value2 = tempvalue2;
+  if(tempvalue3 < flex3max && tempvalue3 > flex3min)
+    value3 = tempvalue3;
+  if(avgcount < 10){
+    avgvalue1 = avgvalue1 + value1;
+    avgvalue2 = avgvalue2 + value2;
+    avgvalue3 = avgvalue3 + value3;
+    
+    avgcount++;
+   // Serial.println(avgcount);
+  }
+
+  else{
+    avgcount = 0;
+    avgvalue1 = avgvalue1 / 10;
+    avgvalue2 = avgvalue2 / 10;
+    avgvalue3 = avgvalue3 / 10;
+
+    
+//    delay(100);
+  //  Serial.println("avg");
+ // Serial.println(avgvalue1);
+//  Serial.println(avgvalue2);
+//  Serial.println(avgvalue3);
+  //Serial.println("------------");
+
+    int mapavgvalue1 = map(avgvalue1 , flex1min , flex1max , 10 , 240);
+    int mapavgvalue2 = map(avgvalue2 , flex2min , flex2max , 10 , 240);
+    int mapavgvalue3 = map(avgvalue3 , flex3min , flex3max , 10 , 240);
+    avg[0] = mapavgvalue1;
+    avg[1] = mapavgvalue2;
+    avg[2] = mapavgvalue3;
+    avgvalue1 = 0;
+    avgvalue2 = 0;
+    avgvalue3= 0;
+
+  }
+lastana = millis();
+
+if(!digitalRead(2)) avg[3] = 100;
+if(!digitalRead(3)) avg[3] = 1;
+if(!digitalRead(4)) avg[3] = 2;
+if(!digitalRead(5)) avg[3] = 3;
+if(!digitalRead(6)) avg[3] = 4;
+if(!digitalRead(7)) avg[3] = 5;
+if(!digitalRead(8)) avg[3] = 6;
+//if(digitalRead(7) &&digitalRead(2) &&digitalRead(3) &&digitalRead(4) &&digitalRead(5) &&digitalRead(6))  avg[3] = 100;
+
+
+}//analog millis sonu
+//for(int i=0; i<4;i++){
+//  Serial.println(avg[i]);
+//   
+//  delay(10);
+//}
+//Serial.println("------------");
+}//loop sonu
+
+
+
